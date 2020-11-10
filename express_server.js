ec92ed1b980d795208e8069121d830898f5586c7
Samantha Fok <samantha.fok@hotmail.com>
2020-11-10 04:11:50 +0000

    Generated random string for shortURL and redirected user to new page with shortURL and longURL

--- express_server.js
+++ express_server.js
@@ -6,10 +6,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = function() {
+  return Math.floor((1 + Math.random()) * 0x100000).toString(16).substring();
 };
 
 app.get("/", (req, res) => {
@@ -24,6 +27,20 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  let shortie = generateRandomString();
+  let longie = req.body.longURL;
+  urlDatabase[shortie] = longie;
+  // const templateVars = { shortURL: req.params.shortie, longURL: req.body.longie }
+  res.redirect(`/urls/${shortie}`); 
+  // res.render("urls_show", templateVars)  
+});
+
+app.get(`/u/:shortURL`, (req, res) => {
+  const longURL = urlDatabase[shortie]
+  res.redirect(longURL);
+});
+
 app.get("/urls", (req,res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
@@ -34,7 +51,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
