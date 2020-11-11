76d4923eccd7d9c880d5e182af93098abafd793b
Samantha Fok <samantha.fok@hotmail.com>
2020-11-11 06:07:47 +0000

    Made add.post to take make login and assign username to cookie

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -25,14 +28,25 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//LOGIN
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+
 //INDEX OF URLS
 app.get("/urls", (req,res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+  console.log(templateVars)
   res.render("urls_index", templateVars);
 });
+
 //NEW URL PAGE
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 //CREATE NEW SHORT URL
@@ -49,7 +63,7 @@ app.get(`/u/:shortURL`, (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -62,12 +76,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //EDIT A URL
 app.post("/urls/:shortURL", (req, res) => {
-  console.log(req.params, req.body)
+//req.params = shortURL, req.body = longURL
   let newLong = req.body.longURL;
-  console.log(newLong)
   urlDatabase[req.params.shortURL] = newLong;
-  console.log(urlDatabase)
-  res.redirect(`/urls`)
+  res.redirect(`/urls`);
 });
 
 //LISTEN
