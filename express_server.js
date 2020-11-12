8e8f878cd31573258661de34daf991c8009b11e9
Samantha Fok <samantha.fok@hotmail.com>
2020-11-12 21:37:46 +0000

    Added bcrypt module and modified /register to encrypt new user's passwords

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan')
 const app = express();
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const bcrypt = require('bcrypt');
 
 const PORT = 8080;
 const bodyParser = require("body-parser");
@@ -10,6 +11,8 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
+
+//~~~~~~~~~~~~~FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~
 
 const generateRandomString = function() {
   return Math.floor((1 + Math.random()) * 0x100000).toString(16).substring();
@@ -25,7 +28,7 @@ const urlsForUser = (id) => {
   return userURL;
 }
 
-
+//~~~~~~~~~~~~~DATABASES~~~~~~~~~~~~~~~~~~~~~~
 const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "idString"},
   "9sm5xK": {longURL: "http://www.google.com", userID: "idString"},
@@ -36,19 +39,19 @@ const userDB =  {
   "idString" : {
     id: "idString",
     email: "kermit@thefrog.com",
-    password: "123",
+    password: bcrypt.hashSync("password", 10),
   },
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 //LOGIN
 app.post("/login", (req, res) => {
@@ -97,12 +100,14 @@ app.post("/register", (req, res) => {
   //loop through the list of user id's in the database
   //maybe make into a function?
   for (const id in userDB) {
-    if (userDB[id].email === email) {
+    if (userDB[id].email === email) { //If email matches one in DB
       return res.status(400).send("This email is taken! Please use a different email.");
     }
-  }
+  } //If there's no email that matches one in DB
+
+  const hashPass = bcrypt.hashSync(password, 10);
   const id = generateRandomString();
-  const user = {id, email, password};
+  const user = {id, email, hashPass};
   userDB[id] = user;
   res.cookie("user_ID", userDB[id])
   res.redirect("urls");
@@ -114,7 +119,6 @@ app.get("/urls", (req,res) => {
     res.redirect("/login").send("Please log in first.");
   }
   let userCollection = urlsForUser(req.cookies["user_ID"].id);
-  console.log(userCollection)
   const templateVars = {urls: urlDatabase, user: req.cookies["user_ID"], userSpecific: userCollection};
   res.render("urls_index", templateVars);
 });
